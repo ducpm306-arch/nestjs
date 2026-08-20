c9340af99218717776ac5f63ed5ab589c8fa6b67
ducpm306-arch <ducpm306@gmail.com>
2026-08-20 17:36:02 +0700

    quak

---
+++ src/modules/products/product.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Delete, Get, Post, Put } from "@nestjs/common";
+
+@Controller('products')
+export class ProductController {
+    @Get()
+    getProducts() {
+        return 'GET LIST PRODUCTS';
+    }
+
+    @Post()
+    createProduct(): string {
+        return 'POST LIST PRODUCT';
+    }
+
+    @Get('/:id')
+    detailProduct(): string {
+        return 'DETAIL PRODUCT';
+    }
+
+    @Put('/:id')
+    updateProduct(): string {
+        return 'UPDATE PRODUCT';
+    }
+
+    @Delete('/:id')
+    deleteProduct(): string {
+        return 'DELETE PRODUCT';
+    }
+
+}
+
